7397d47c3c0259a54bc3679a4ee38e1f6d421397
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-05-28 08:08:01 +0000

    install external module and use of it

---
+++ External_module.py
@@ -0,0 +1,8 @@
+# open termianl
+# pip install pyttsx3(which convert text to specch)
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("This code shows us how to install external module and use of it")
+engine.runAndWait()
+
