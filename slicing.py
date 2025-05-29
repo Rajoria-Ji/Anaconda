6fb2c319c8113e40c10669754ef159701affe8b0
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-05-29 19:05:25 +0000

    slicing

---
+++ slicing.py
@@ -0,0 +1,9 @@
+a = "123456"
+
+print(a[0:]) #slicing (0 index to the end of the string)
+print(a[:]) #slicing (0 index to the end of the string)
+print(a[0:5]) # print(a[:5]) slicing ( 0 index to the 5th index,where 5th position value is not included)
+
+#Negative slicing
+#123456
+print(a[-4:-1])
