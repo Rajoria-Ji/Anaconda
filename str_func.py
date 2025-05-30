3ee660965f3d9eb1f2b9ba9b9747c070c2922e47
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-05-30 06:51:19 +0000

    useful function of string

---
+++ str_func.py
@@ -0,0 +1,17 @@
+name = " sumit are "
+
+print(len(name)) #print the lenght of the string
+print(name.startswith('sum')) #check the string starts with the entered string 
+print(name.endswith("man"))#check the string starts with the entered string 
+print(name.capitalize())#capitalize the 1st letter of the string
+print(name.lower()) 
+print(name.upper())
+print(name.title())
+print(name.split())
+print(name.strip())
+print(name.replace("t","n"))
+print(name.find(" "))
+print(name.count("s"))
+print(name.isalpha()) # Check the string is alphabetical or not
+print(name.isnumeric()) #Check the string is numeric or not
+print(name.isalnum()) #check the string is alphabet or numberic(without special charater)
