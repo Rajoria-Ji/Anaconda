7bc59dd3a9a10469f35b866f5328c8553d5f0b5b
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-05-31 18:16:25 +0000

    string function use

---
+++ str_practice.py
@@ -0,0 +1,15 @@
+'''n = input("Enter your name: ")
+
+print("Good after noon",n)
+print("Good after noon",n.replace(":",n))
+print(f"Good after noon {n}")'''
+
+# l='''Dear <|Name|>
+# you are selected !
+# <|Date>|'''
+# n="sameer"
+# print(l.replace("Name",n).replace("Date","12/05/25"))
+
+
+'''letter = "Dear harry\n\tthis python course is nice.\nthanks"
+print(letter)''' # escape sequence
