65a7ad245373ba89970a34a8ad4e82e82a2d8513
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-06-19 07:19:08 +0000

    Methods of dictionary

--- dict.py
+++ dict.py
@@ -30,6 +30,7 @@ print(a)      #prints value of that removed key
 b=dic.popitem() #remove the last key from the dictionary
 print(b)    #returns the last key value pair
 print(c)    #it will paste the copied dictionary 
+print(len(dic))     # it will print the lenth of the dictionary 
 print(dic.setdefault(6)) #prints a value of key, None if not found
 #print(dic[6])     #Returns an error
 # dic.clear()  #remove all items from the dictionary
