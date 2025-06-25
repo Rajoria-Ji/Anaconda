b7f31d88500fefa57cf88744c72c516afd72ccb4
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-06-25 16:53:18 +0000

    some question of dictionary and set

---
+++ prob_dic.&.set.py
@@ -0,0 +1,71 @@
+# #question 1 
+
+# dic = {"madad":"help",
+# "kutta":"dog",
+# }
+
+
+# word = input("Enter the word you want to meaning of: ")
+
+# print(dic[word])
+
+# # question 2
+
+# se =set()
+
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# number=int(input("Enter a number:"))
+# se.add(number)
+# print(se)
+
+# question 3
+
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')
+# print(len(s))   == 2
+
+#question 4
+
+# dic ={}
+
+# name = input("Enter your name: ")
+# lang = input("Enetr you favourite:")
+
+# dic.update({name : lang})
+# name = input("Enter your name: ")
+# lang = input("Enetr you favourite:")
+
+# dic.update({name : lang})
+# name = input("Enter your name: ")
+# lang = input("Enetr you favourite:")
+
+# dic.update({name : lang})
+# name = input("Enter your name: ")
+# lang = input("Enetr you favourite:")
+
+# dic.update({name : lang})
+
+# print(dic)
+
+#question
+
+# can we change the value of list in set ??
+
+# s = {8,7,12,"Harry",[1,2]}
+
+# we can not change even we can not include list in a set
