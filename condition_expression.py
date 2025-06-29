39c985ce2dbe8a8b07f21b95c12e20bf4c8fbc79
Sumit Rajoria <183298325+Rajoria-Ji@users.noreply.github.com>
2025-06-29 09:16:39 +0000

    Some basic problem of comnditional expression

---
+++ condition_expression.py
@@ -0,0 +1,61 @@
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# c=int(input("Enter a number: "))
+# d=int(input("Enter a number: "))
+# 121
+
+# if(a>b and a>c and a>d):
+#     print(a,"is a greatest number")
+# elif(b>a and b>c and b>d):
+#     print(b,"is a greatest number")
+# elif(c>b and c>a and c>d):
+#     print(c,"is a greatest number")
+# else:
+#     print(d,"is a greatest number")
+
+
+
+
+# m1=int(input("Enter marks of 1st subject: "))
+# m2=int(input("Enter marks of 2nd subject: "))
+# m3=int(input("Enter marks of 3rd subject: "))
+
+# if((m1+m2+m3)/3>=40 and m1>32 and m2>32 and m3>32):
+#     print("pass")
+# else:it
+#     print("failed")
+
+
+
+'''write a program to find whether a given username cointains less than 10 characters or not'''
+
+
+
+# user=input("Enter a user name: ")
+
+# if len(user)>10:
+#     print("Not")
+# else:
+#     print("User name cointains less than 10 characters")
+
+# '''Question'''
+# '''check out your name is there or not'''
+
+# letter=input("Enter a name: ")
+
+# if ("Sumit".upper() in letter.upper()):
+#     print("There is your name")
+# else:
+#     print("your name is not there")
+
+
+
+# '''Check whether a given name is presented in a list or not.'''
+
+
+# lis=["Sumit","bhanu","Himanshu"]
+# name=input("Enter your name:")
+# if name in lis:
+#     print("Congrats,Your are selected.")
+# else:
+#     print("Sorry,Your are not selected")
